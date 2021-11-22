de09b2129c6daa5bf172e758f0a5b3a8a1a1fd34
Andrea Barisani <andrea@inversepath.com>
2021-11-22 16:39:45 +0100

    add example functions for RNGB yielding

---
+++ trusted_os/rng.go
@@ -0,0 +1,47 @@
+// Copyright (c) F-Secure Corporation
+// https://foundry.f-secure.com
+//
+// Use of this source code is governed by the license
+// that can be found in the LICENSE file.
+
+package main
+
+import (
+	"fmt"
+
+	"github.com/canonical/go-sp800.90a-drbg"
+
+	"github.com/f-secure-foundry/tamago/soc/imx6"
+	"github.com/f-secure-foundry/tamago/soc/imx6/rngb"
+)
+
+// yieldRNGB re-configures the TamaGo runtime entropy source to a pure software
+// one (NIST SP 800-90A DRBG) to allow scenarios where it is desirable to give
+// free RNGB access to the Normal World OS.
+func yieldRNGB() {
+	seed := make([]byte, 256)
+	rngb.GetRandomData(seed)
+
+	nonce := make([]byte, 128)
+	rngb.GetRandomData(nonce)
+
+	uid := imx6.UniqueID()
+
+	rng, err := drbg.NewCTRWithExternalEntropy(32, seed, nonce, uid[:], nil)
+
+	if err != nil {
+		panic(fmt.Sprintf("could not instantiate DRBG, %v", err))
+	}
+
+	// override TamaGo entropy source with an RNGB seeded DRGB
+	imx6.SetRNG(func(b []byte) {
+		rng.Read(b)
+	})
+
+	rngb.Reset()
+}
+
+// restoreRNGB restores the RNGB as TamaGo runtime entropy source.
+func restoreRNGB() {
+	imx6.SetRNG(rngb.GetRandomData)
+}
