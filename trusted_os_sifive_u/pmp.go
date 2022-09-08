0d82f80900ff0a08f24d1314340612aa72de67b0
Andrea Barisani <andrea@inversepath.com>
2022-09-08 16:59:12 +0200

    workaround for qemu bug in PMP handling no longer necessary

--- trusted_os_sifive_u/pmp.go
+++ trusted_os_sifive_u/pmp.go
@@ -20,59 +20,26 @@ const (
 	smEnd   = mem.SecureStart + mem.SecureSize + mem.SecureDMASize
 )
 
-func protectSM(i int) (int, error) {
-	if err := fu540.RV64.WritePMP(i, smStart, true, true, true, riscv.PMP_CFG_A_OFF, false); err != nil {
-		return -1, err
-	}
-	i += 1
-
-	if err := fu540.RV64.WritePMP(i, smEnd, false, false, false, riscv.PMP_CFG_A_TOR, false); err != nil {
-		return -1, err
-	}
-	i += 1
-
-	return i, nil
-}
-
-func configurePMP(ctx *monitor.ExecCtx) (err error) {
-	var i int
-
-	ctxStart := ctx.Memory.Start()
-	ctxEnd := ctx.Memory.End()
-
+func configurePMP(ctx *monitor.ExecCtx, i int) (err error) {
 	// grant full peripheral access
-	if err = fu540.RV64.WritePMP(i, 0x80000000, true, true, true, riscv.PMP_CFG_A_TOR, false); err != nil {
+
+	if err = fu540.RV64.WritePMP(i, 0x00000000, true, true, true, riscv.PMP_CFG_A_OFF, false); err != nil {
 		return
 	}
-	i += 1
 
-	// Due to PMP priority evaluation the restriction for Secure Monitor
-	// region is prioritized based on its location respective to the
-	// execution context memory.
-	switch {
-	case ctxStart >= smEnd:
-		if i, err = protectSM(i); err != nil {
-			return
-		}
-	case ctxEnd <= smStart:
-		defer func() {
-			_, err = protectSM(i)
-		}()
-	default:
-		panic("invalid memory layout")
-	}
-
-	// grant execution context memory region
-
-	if err = fu540.RV64.WritePMP(i, uint64(ctxStart), true, true, true, riscv.PMP_CFG_A_OFF, false); err != nil {
+	if err = fu540.RV64.WritePMP(i+1, 0x80000000, true, true, true, riscv.PMP_CFG_A_TOR, false); err != nil {
 		return
 	}
-	i += 1
 
-	if err = fu540.RV64.WritePMP(i, uint64(ctxEnd), true, true, true, riscv.PMP_CFG_A_TOR, false); err != nil {
+	// protect Security Monitor
+
+	if err = fu540.RV64.WritePMP(i+2, smStart, true, true, true, riscv.PMP_CFG_A_OFF, false); err != nil {
 		return
 	}
-	i += 1
+
+	if err = fu540.RV64.WritePMP(i+3, smEnd, false, false, false, riscv.PMP_CFG_A_TOR, false); err != nil {
+		return
+	}
 
 	// TODO: IOPMP
 
